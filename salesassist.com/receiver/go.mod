0e3ede945bcddfab54a4ed0db8e65d75eaf89e43
Renato Leite <renatoleite@google.com>
2020-08-28 23:56:00 -0300

    Put everything under Receiver API

--- salesassist.com/receiver/go.mod
+++ salesassist.com/receiver/go.mod
@@ -7,4 +7,5 @@ require (
 	cloud.google.com/go/pubsub v1.3.1
 	github.com/golang/gddo v0.0.0-20200715224205-051695c33a3f
 	github.com/gorilla/mux v1.7.4
+	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98
 )
--- salesassist.com/receiver/receiverapi.go
+++ salesassist.com/receiver/receiverapi.go
@@ -6,14 +6,19 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/golang/gddo/httputil/header"
 	"github.com/gorilla/mux"
+
+	dialogflow "cloud.google.com/go/dialogflow/apiv2"
+	dialogflowpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
 )
 
 var projectID string
@@ -32,6 +37,71 @@ func init() {
 		log.Printf("pubsub.NewClient: %v", err)
 		return
 	}
+}
+
+// Entities for query reference
+type Entities struct {
+	Entities []EntitySynonyms `json:"entities"`
+}
+
+// EntitySynonyms key:value pair
+type EntitySynonyms struct {
+	Entity   string   `json:"entity"`
+	Synonyms []string `json:"synonyms"`
+}
+
+func (e *Entities) loadDictFromFile(filePath string) {
+
+	// Open our jsonFile
+	jsonFile, err := os.Open(filePath)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Successfully Opened json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &e)
+
+}
+
+func detectIntentText(projectID string, sessionID string, intents map[string]string, languageCode string) map[string]string {
+	intentResponse := make(map[string]string)
+	ctx := context.Background()
+
+	sessionClient, err := dialogflow.NewSessionsClient(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer sessionClient.Close()
+
+	if projectID == "" || sessionID == "" {
+		fmt.Println(projectID, sessionID)
+	}
+
+	sessionPath := fmt.Sprintf("projects/%s/agent/sessions/%s", projectID, sessionID)
+
+	for key := range intents {
+		textInput := dialogflowpb.TextInput{Text: key, LanguageCode: languageCode}
+		queryTextInput := dialogflowpb.QueryInput_Text{Text: &textInput}
+		queryInput := dialogflowpb.QueryInput{Input: &queryTextInput}
+		request := dialogflowpb.DetectIntentRequest{Session: sessionPath, QueryInput: &queryInput}
+
+		response, err := sessionClient.DetectIntent(ctx, &request)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		queryResult := response.GetQueryResult()
+		fulfillmentText := queryResult.GetFulfillmentText()
+
+		intentResponse[key] = fulfillmentText
+	}
+
+	return intentResponse
 }
 
 type malformedRequest struct {
@@ -111,6 +181,59 @@ type Payload struct {
 	End        string `json:"end"`
 }
 
+func (p *Payload) findSynonyms(entities Entities) map[string]string {
+
+	match := make(map[string]string)
+
+	for _, entity := range entities.Entities {
+		for _, synonym := range entity.Synonyms {
+			isMatchSynonym, _ := regexp.MatchString(synonym, p.Transcript)
+			if isMatchSynonym {
+				match[entity.Entity] = synonym
+				break
+			}
+		}
+	}
+
+	return match
+}
+
+// Responses with intent matching
+type Responses struct {
+	Responses []ResponseContent `json:"response"`
+}
+
+// ResponseContent with intent matching
+type ResponseContent struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+func (resp *Responses) buildResponseContent(intentResponses map[string]string) {
+	for key, element := range intentResponses {
+		resp.Responses = append(resp.Responses, ResponseContent{key, element})
+	}
+}
+
+// History to be published
+type History struct {
+	MeetingID  string            `json:"meetingid"`
+	Speaker    string            `json:"phrase"`
+	Transcript string            `json:"transcript"`
+	Start      string            `json:"start"`
+	End        string            `json:"end"`
+	Responses  []ResponseContent `json:"response"`
+}
+
+func (hist *History) buildHistoryContent(p *Payload, resp *Responses) {
+	hist.MeetingID = p.MeetingID
+	hist.Speaker = p.Speaker
+	hist.Transcript = p.Transcript
+	hist.Start = p.Start
+	hist.End = p.End
+	hist.Responses = resp.Responses
+}
+
 func payloadCreate(w http.ResponseWriter, r *http.Request) {
 	var p Payload
 
@@ -126,7 +249,7 @@ func payloadCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := &Payload{Phrase: p.Phrase, MeetingID: p.MeetingID}
+	msg := &Payload{Transcript: p.Transcript, MeetingID: p.MeetingID}
 	msgByte, err := json.Marshal(msg)
 	publish(msgByte)
 
